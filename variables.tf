4a23bfd5266bc6528da029e98151d9b205006681
Alex Kondratiev <7284572+cSkyHawk@users.noreply.github.com>
2026-09-08 18:08:49 -0400

    Types ssh_port and orders the variable blocks

--- variables.tf
+++ variables.tf
@@ -2,44 +2,45 @@
 # Variables
 #-----------------------------------------------------------------------------------------------------------------------
 variable "name" {
+  description = "Name of the firewall"
   type        = string
   default     = "my-firewall"
-  description = "Name of the firewall"
-
 }
+
 variable "ssh_source_ips" {
+  description = "Source networks that have SSH access to the servers"
   type        = list(string)
   default     = ["0.0.0.0/0", "::/0"]
-  description = "Source networks that have SSH access to the servers"
 }
 
 variable "ssh_port" {
-  default     = 22
   description = "TCP port the incoming SSH rule opens"
+  type        = number
+  default     = 22
 }
 
 variable "allow_egress_traffic" {
+  description = "Whether or not to allow the egress traffic"
   type        = bool
   default     = true
-  description = "Whether or not to allow the egress traffic"
 }
 
 variable "allow_ingress_web" {
+  description = "Whether or not to allow the ingress WEB traffic"
   type        = bool
   default     = false
-  description = "Whether or not to allow the ingress WEB traffic"
 }
 
 variable "block_icmp_ping_in" {
+  description = "Block ingress ICMP"
   type        = bool
   default     = false
-  description = "Block ingress ICMP"
 }
 
 variable "extra_firewall_rules" {
+  description = "Additional firewall rules to apply to the cluster"
   type        = list(any)
   default     = []
-  description = "Additional firewall rules to apply to the cluster"
 }
 
 variable "labels" {
